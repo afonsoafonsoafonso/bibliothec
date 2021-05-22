c3b565f01f745c01ac2768a38fe7941a88279314
Joaquim Rodrigues <jmscrodrigues@hotmail.com>
2021-05-22 18:40:50 +0100

    Added InfoGraph with functions

--- app/bibliothec/src/components/graph/InfoGraph.js
+++ app/bibliothec/src/components/graph/InfoGraph.js
@@ -1,111 +1,152 @@
-import React, { useEffect } from 'react';
+import React, {Component} from 'react';
+//import ReactDOM from "react-dom";
 import Graph from "react-graph-vis";
 
-function InfoGraph({ nodesProps, edgesProps }) {
-
-    let network = undefined;
-    const options = {
-        layout: {
-            // hierarchical: true,
-        },
-        edges: {
-            color: "#000000"
-        },
-        nodes: {
-            widthConstraint: 50,
-            fixed: {
-                x: false,
-                y: false
+class InfoGraph extends Component {
+    constructor() {
+        super();
+        this.state = {
+          options: {
+            layout: {
+              // hierarchical: true,
             },
-        },
-        physics: {
-            enabled: true,
-            barnesHut: {
-                springConstant: 0.015,
-                avoidOverlap: 0.02
+            edges: {
+              color: "#000000"
+            },
+            nodes: {
+                widthConstraint: 50,
+                fixed: {
+                    x: false,
+                    y: false
+                },
+            },
+            physics: {
+                enabled: true,
+                barnesHut: {
+                    springConstant: 0.015,
+                    avoidOverlap: 0.02
+                }
+            },
+            groups: {
+                Authors: {color:{background:'#cc0052', border:'#b30047', highlight: {background:'#b30047', border:'#99003d'}}, borderWidth:1, shape:'dot'},
+                Publishers: {color:{background:'#29a329', border:'#248f24', highlight: {background:'#248f24', border:'#1f7a1f'}}, borderWidth:1, shape:'dot'},
+                Books: {color:{background:'#005ce6', border:'#0052cc', highlight: {background:'#0052cc', border:'#0047b3'}}, borderWidth:1, shape:'dot'},
+            },
+            interaction: { multiselect: false, dragView: true }
+          },
+          
+          graph: {
+            nodes: [
+              { id: 10, group:'Authors', label: "Node ssssss1", x:0, y:0},
+              { id: 2, group:'Authors', label: "Node sssss222s1", x:100, y:100}
+
+            ],
+            edges: [
+              { from: 1, to: 2 },
+              { from: 1, to: 3 },
+              { from: 2, to: 4 },
+              { from: 2, to: 5 }
+            ]
+          },
+          counter: 7,
+          events: {
+            selectNode: ({nodes}) => {
+              console.log("Selected nodes:");
+              console.log(nodes);
+            },
+            doubleClick: ({ nodes }) => {
+              if(nodes.length !== 0) {
+                this.resetNodes(nodes[0]);
+                this.addNodes();
+              }
             }
-        },
-        groups: {
-          Authors: {color:{background:'#cc0052', border:'#b30047', highlight: {background:'#b30047', border:'#99003d'}}, borderWidth:1, shape:'dot'},
-          Publishers: {color:{background:'#29a329', border:'#248f24', highlight: {background:'#248f24', border:'#1f7a1f'}}, borderWidth:1, shape:'dot'},
-          Books: {color:{background:'#005ce6', border:'#0052cc', highlight: {background:'#0052cc', border:'#0047b3'}}, borderWidth:1, shape:'dot'},
-        },
-        interaction: { multiselect: false, dragView: true }
-    };
-
-    let graph = {
-        nodes: [],
-        edges: []
-    };
-    
-    useEffect(() => {
-        if (nodesProps) {
-
-            let nodes = nodesProps.map(
-                (item) =>
-                ({ id: item.id.toString(), label: item.label.toString(), group: item.group.toString()}
-            ));
-
-            nodes.forEach(node => {
-                graph.nodes.push(node);
-            });
-        }
-        else {
-            graph.nodes = [];
-        }}, [nodesProps, graph]); 
-
-    useEffect(() => {
-        if (edgesProps) {
-           let edges = edgesProps.map((item, i) => ({ id: i.toString(), from: item.source.toString(), to: item.target.toString()}));
-           edges.forEach(edge => {
-               graph.edges.push(edge);
-           });
-        }
-        else {
-            graph.edges = [];
-        }
-    }, [edgesProps, graph]); 
-
-    const getChosenNode = (id) => {
-        return network?.getPosition(id);
+          }
+        };
     }
 
-    const events = {
-        selectNode: ({ nodes }) => {
-            console.log("Selected nodes:");
-            console.log(nodes[0]);
-            let currNode = getChosenNode(nodes[0]);
-            console.log(currNode?.x);
-            console.log(currNode?.y);
-        },
-        doubleClick: ({ nodes }) => {
-            let currNode = getChosenNode(nodes[0]);
-            console.log(currNode?.x);
-            console.log(currNode?.y);
-        }
+    addNodes() {
+        this.setState(({ graph: { nodes, edges }, counter, ...rest}) => {
+            const id = counter + 1;
+            return {
+              graph: {
+                nodes: [
+                  ...nodes,
+                  ...this.props.nodesProps
+                ],
+                edges: [
+                  ...edges,
+                  ...this.props.edgesProps
+                ]
+              },
+              counter: id,
+              ...rest
+            }
+          });
     }
 
-    console.log(graph);
-    
-    let component = (
-        <div id="graph" style={{ height: "100vh" }}>
+    resetNodes(n) {
+        let nodes_copy = [...this.state.graph.nodes];
+        let node_copy = nodes_copy.filter(node => {
+            return node.id === n
+        });
+        console.log(node_copy);
+        this.setState(({ graph: {  }, counter, ...rest}) => {
+            return {
+              graph: {
+                nodes: [
+                  ...node_copy
+                ],
+                edges: [
+                ]
+              },
+              counter: 1,
+              ...rest
+            }
+          });
+    }
+
+    /*
+    Provavelmente em vez de nx e ny, apenas um incremento / decremento
+    */
+
+    updatePosition(id, nx, ny) {
+      this.setState(({graph:{nodes}}) =>{
+        return {
+          graph: {
+            nodes: [
+              ...nodes.slice(0, id - 1),
+              {
+                ...nodes[id -1],
+                x: nx,
+                y: ny
+              },
+              ...nodes.slice(id)
+            ]
+          }
+
+        }
+      });
+    }
+
+
+    getChosenNode(id){
+      return this.state.graph.nodes[id - 1];
+    } 
+  
+
+    render() {
+        return (
+          <div id="graph" style={{ height: "100vh" }}>
             <Graph
-                graph={graph}
-                options={options}
-                events={events}
-                getNetwork={networkVis => {
-                    //  if you want access to vis.js network api you can set the state in a parent component using this property
-                    network = networkVis;
-                    network.stabilize(100);
-                }}
+              graph={this.state.graph}
+              options={this.state.options}
+              events={this.state.events}
             />
-        </div>   
-    ) 
+          </div>
+        );
+    }
 
-    return (
-        component
-    );
-};
+}
 
 
 export default InfoGraph;
