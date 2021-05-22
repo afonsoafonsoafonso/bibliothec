cba2aa4bb251bd5c350f49fd35aba33969b722d7
Joaquim Rodrigues <jmscrodrigues@hotmail.com>
2021-05-22 18:41:30 +0100

    Added Viewer

--- app/bibliothec/src/components/Viewer.js
+++ app/bibliothec/src/components/Viewer.js
@@ -1,7 +1,5 @@
 import React, { useEffect } from 'react';
 import InfoGraph from './graph/InfoGraph';
-import { withRouter } from 'react-router-dom'
-import ReactDOM from "react-dom";
 
 function Viewer(props) {
     const [state, setState] = React.useState({
@@ -11,17 +9,18 @@ function Viewer(props) {
 
     useEffect(() => {
         setState({
-            ...state, loading: false, graph_info: {"nodes": [{"id": 0,"label": "Node 0", "group": "Authors"}, 
-            {"id": 1,"label": "Node 1", "group": "Books"}, {"id": 2,"label": "Node 2", "group": "Publishers"},
-            {"id": 3,"label": "Node 3", "group": "Authors"}, {"id": 4,"label": "Node 4", "group": "Books"}],
-            "links": [{"source": 0, "target": 1}, {"source": 0, "target": 2}, 
-            {"source": 0, "target": 3}, {"source": 0, "target": 4}]}
+            ...state, loading: false, graph_info: {"nodes": [{"id": 2,"label": "Node 0", "group": "Authors"}, 
+            {"id": 4,"label": "Node 1", "group": "Books"}, {"id": 3,"label": "Node 2", "group": "Publishers"},
+            {"id": 5,"label": "Node 3", "group": "Authors"}, {"id": 6,"label": "Node 4", "group": "Books"}],
+            "links": [{"source": 1, "target": 2}, {"source": 1, "target": 3}, 
+            {"source": 1, "target": 4}, {"source": 1, "target": 5}]}
         });
     }, []);
-
+    
     let component = state.loading ? "loading" :
-        <InfoGraph nodesProps={state.graph_info.nodes} edgesProps={state.graph_info.links}/>
-    return component ;
+        <InfoGraph nodesProps={state.graph_info.nodes} edgesProps={state.graph_info.links} />
+    return component;
 }
+
 
 export default Viewer;
